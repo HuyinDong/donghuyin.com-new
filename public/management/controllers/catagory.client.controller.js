d90cb72cdf691c5a88ec7bc0d1aeeff10380a499
Huyin Dong <dejorkoffe@gmail.com>
2015-08-29 12:18:03 -0400

    add insert api

--- public/management/controllers/catagory.client.controller.js
+++ public/management/controllers/catagory.client.controller.js
@@ -1,3 +1,19 @@
 /**
  * Created by dongyin on 8/27/15.
  */
+management.controller('catagoryController',function($scope,$http,$rootScope) {
+    var tran = {};
+    tran.table = "newsbase";
+    tran.obj = {
+        cid : 30,
+        title : 'test',
+        author : 'Chris',
+
+        abstract : 'test'
+    };
+    $scope.insert = function(){
+        $http.post('/management/data', tran).then(function (data) {
+            console.log(data);
+        });
+    }
+});
