56885ea827d072cc6c8197d7cf86d0143162690d
Huyin Dong <dejorkoffe@gmail.com>
2015-09-02 16:39:41 -0400

    n

--- public/management/controllers/list.client.controller.js
+++ public/management/controllers/list.client.controller.js
@@ -38,18 +38,16 @@ management.controller('listController',
         //set gridApi on scope
         $scope.gridApi = gridApi;
         gridApi.selection.on.rowSelectionChanged($scope,function(row){
-            if(row.isSelected){
-                $scope.isSelected = true;
-            }else{
-                $scope.isSelected = false;
-            }
+            $scope.isSelected = row.isSelected;
             transferredId = row.entity.id;
         });
     };
 
 
     $scope.editLine = function(){
-        $state.go('edit',{transferredId : transferredId});
+        $timeout(function () {
+            $state.go('edit', {transferredId: transferredId});
+        },1000);
     }
 
     $scope.deleteLine = function(){
