f137014d0a86d2ae12c443caac69fe2e78a9021d
Huyin Dong <dejorkoffe@gmail.com>
2015-09-11 09:19:46 -0400

    adjust postion of articles

--- public/content/content.client.controller.js
+++ public/content/content.client.controller.js
@@ -4,21 +4,24 @@
 content.controller('contentController',function($scope,$http,$stateParams){
     if(typeof $stateParams.id == 'undefined'){
         $http.get('/data/base/').then(function (data) {
-            console.log(data.data);
             $scope.articles = data.data;
             $scope.name = "Latest Articles";
-            $scope.quantity = 10;
+            $scope.quantity = 7;
         });
     }else {
         $http.get('/data/base/' + $stateParams.id).then(function (data) {
             $scope.articles = data.data;
             $scope.name = $stateParams.name;
-            console.log(data.data);
         });
     }
+    $scope.showMore = function(){
+        $scope.quantity = $scope.quantity+4;
+    };
+
 });
 content.filter('reverse', function() {
     return function(items) {
+        if (!angular.isArray(items)) {return false;}
         return items.slice().reverse();
     };
 });
